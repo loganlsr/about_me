926f6295e78fc13debee0e062ae0055003295264
Logan Rogers <logan.s.rogers@biola.edu>
2016-06-14 15:13:40 -0700

    all questions done

--- app.js
+++ app.js
@@ -13,5 +13,32 @@ else {
 
 var answer2 = prompt('Is Logan\'s favorite color brown?').toLowerCase();
 if (answer2 === 'yes' || answer2 === 'y') {
-  alert('Wrong! That\'s like the worst color. Green is actually his favorite color!')
+  alert('Wrong! That\'s like the worst color. Green is actually his favorite color!');
+}
+else {
+  alert('Correct! It\'s green! Who likes the color brown?');
+}
+
+var answer3 = prompt('Does Logan like to play video games in his free time?').toLowerCase();
+if (answer3 === 'yes' || answer3 === 'y') {
+  alert('Correct! He\'s particularly fond of Overwatch right now');
+}
+else {
+  alert('Incorrect! He has been gaming for forever and he is particularly fond of Overwatch right now!');
+}
+
+var answer4 = prompt('Is Logan\'s favorite food pizza?').toLowerCase();
+if (answer4 === 'yes' || answer4 === 'y') {
+  alert('Nope! His favorite food is actually chinese food. And if you want to get specific, he loves General Tsao\'s Chicken!');
+}
+else {
+  alert('Pizza is really good, but you are right. It is not his favorite food, that would be chinese food, but it is a close second!');
+}
+
+var answer5 = prompt('Is one of Logan\'s favorite hobbies watching anime?').toLowerCase();
+if (answer5 === 'yes' || answer5 === 'y') {
+  alert('Indeed! If you are looking for suggestions, he recommends Sword Art Online or Clannad!');
+}
+else {
+  alert('Wrong! When he\'s not playing video games or doing his homework, he is watching a bunch of anime!')
 }
