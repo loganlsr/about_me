4edba9c0d52665ba2a8d1d009dba6f930e65f81f
Logan Rogers <logan.s.rogers@biola.edu>
2016-06-14 14:55:12 -0700

    first question finished

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-alert('yeaaaa');
+var userName = prompt('Greetings! What is your name?');
+alert('Welcome ' + userName + ', glad you\'re here!');
+
+var answer1 = prompt('Does Logan ride the bus to school?').toLowerCase();
+if (answer1 === 'yes' || answer1 === 'y') {
+  alert('Correct! At least he does on the days he doesn\'t oversleep!');
+}
+else {
+  alert('Wrong! Though, I suppose he does drive on the days he oversleeps and misses the bus.');
+}
+
+var answer2 = prompt('Is Logan\'s favorite color brown?').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y') {
+  alert('Wrong! That\'s like the worst color. Green is actually his favorite color!')
+}
